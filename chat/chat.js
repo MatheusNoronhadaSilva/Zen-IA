3192fc95787746c8823f1edba588b7b3e86673ee
MATHEUS NORONHA DA SILVA <23111820@dsn127m1242375.cfp127aluno.local>
2024-04-30 12:29:29 -0300

    terminando

--- chat/chat.js
+++ chat/chat.js
@@ -7,20 +7,25 @@ requisicao.addEventListener('keyup', function(event){
 
     if(event.keyCode === 13){
 
+        const requisicaoTexto = requisicao.value
+
         const divMensagem = document.createElement('div')
-    divMensagem.classList.add('mensagem')
+        divMensagem.classList.add('mensagem')
 
-    const divEscritor = document.createElement('div')
-    divEscritor.classList.add('escritor')
+        const divEscritor = document.createElement('div')
+        divEscritor.classList.add('escritor')
 
-    const imgEscritor = document.createElement('img')
+        const imgEscritor = document.createElement('img')
+        imgEscritor.src = '../img/image-removebg-preview.png'
 
-    const nomeEscritor = document.createElement('h3')
+        const nomeEscritor = document.createElement('h3')
+        nomeEscritor.textContent = 'You'
 
-    const divTexto = document.createElement('div')
+        const divTexto = document.createElement('div')
     divTexto.classList.add('texto')
 
     const texto = document.createElement('p')
+    texto.textContent = requisicaoTexto
 
     divTexto.appendChild(texto)
     divEscritor.replaceChildren(imgEscritor, nomeEscritor)
